7b0c834ba7ac7d34b01910876662586228a00332
MACS-AR <96332901+MACS-AR@users.noreply.github.com>
2026-08-01 01:24:43 +0300

    Update Firebase config with measurementId
    Added measurementId to Firebase configuration.

--- assets/js/firebase/config.js
+++ assets/js/firebase/config.js
@@ -3,17 +3,19 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
-// استبدل هذه القيم بمفاتيح مشروعك من Firebase
+// إعدادات مصنع المجد للأعلاف (Firebase Config)
 const firebaseConfig = {
-            apiKey: "AIzaSyBgRkceRq7FRbhCevLlULYNy-A5Tl_cr0w",
-            authDomain: "sr-test-c9e06.firebaseapp.com",
-            databaseURL: "https://sr-test-c9e06-default-rtdb.firebaseio.com",
-            projectId: "sr-test-c9e06",
-            storageBucket: "sr-test-c9e06.firebasestorage.app",
-            messagingSenderId: "658396508062",
-            appId: "1:658396508062:web:c56cd84f93daa2e176308f"
-        };
+  apiKey: "AIzaSyBgRkceRq7FRbhCevLlULYNy-A5Tl_cr0w",
+  authDomain: "sr-test-c9e06.firebaseapp.com",
+  databaseURL: "https://sr-test-c9e06-default-rtdb.firebaseio.com",
+  projectId: "sr-test-c9e06",
+  storageBucket: "sr-test-c9e06.firebasestorage.app",
+  messagingSenderId: "658396508062",
+  appId: "1:658396508062:web:c56cd84f93daa2e176308f",
+  measurementId: "G-3ZZV344NDK"
+};
 
+// تهيئة التطبيق والخدمات
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
